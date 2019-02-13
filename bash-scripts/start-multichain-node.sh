6ba2d9cf1ba11742a0a6ebd4cd9a9bc28e210258
Antonio Russo <rjproj@gmail.com>
2019-02-13 14:18:57 +0100

    Fix issue on blockchain name and datadit

--- bash-scripts/start-multichain-node.sh
+++ bash-scripts/start-multichain-node.sh
@@ -1,4 +1,5 @@
 export CHAIN_NAME=$1
-export SEED_NODE_HOST=$2
-export SEED_NODE_PORT=$3
+export CHAIN_DATADIR=$2
+export SEED_NODE_HOST=$3
+export SEED_NODE_PORT=$4
 docker-compose -f ./docker-compose/multichain-node.yml -p $CHAIN_NAME up -d
--- vm-controller.py
+++ vm-controller.py
@@ -10,23 +10,23 @@ BASH_SCRIPTS_FOLDER = os.path.dirname(os.path.realpath(__file__)) + '/bash-scrip
 
 class requestHandler(BaseHTTPRequestHandler):
 
-    VM_VOLUME_FOLDER = './node'
+    BC_DATADIR = sys.argv[1]
 
     NODE_NETWORK_PORT = 7411
     NODE_RPC_PORT = 7410
 
-    start_node_cmd = 'start-multichain-node.sh %s %s %s'
+    start_node_cmd = '/start-multichain-node.sh %s %s %s %s'
     start_seed_cmd = 'docker-compose up -d'
 
     # STARTS THE SEED NODE
     def do_POST(self):
-        if not os.path.exists('./node'):
-            os.makedirs('./node')
-        with open(self.VM_VOLUME_FOLDER + '/params.dat', 'w') as outputlog:
+        if not os.path.exists(self.BC_DATADIR):
+            os.makedirs(self.BC_DATADIR)
+        with open(self.BC_DATADIR + '/params.dat', 'w') as outputlog:
             data = self.rfile.read(int(self.headers['Content-length'])).decode('utf-8')
             outputlog.write(data)
             outputlog.close()
-        with open(self.VM_VOLUME_FOLDER + '/multichain.conf', 'w') as conf:
+        with open(self.BC_DATADIR + '/multichain.conf', 'w') as conf:
             conf.write('rpcuser=multichainrpc\n')
             conf.write('rpcpassword=multichainpassword\n')
             conf.write('rpcallowip=0.0.0.0/0')
@@ -42,17 +42,19 @@ class requestHandler(BaseHTTPRequestHandler):
     # STARTS A NODE
     # Passi il params.dat e avvia il nodo connettendolo all'ip fornito nell'header
     def do_PUT(self):
-        with open(self.VM_VOLUME_FOLDER + '/params.dat', 'w') as outputlog:
+        if not os.path.exists(self.BC_DATADIR):
+            os.makedirs(self.BC_DATADIR)
+        with open(self.BC_DATADIR + '/params.dat', 'w') as outputlog:
             data = self.rfile.read(int(self.headers['Content-length'])).decode('utf-8')
             outputlog.write(data)
             outputlog.close()
         result = {}
-        with open(self.VM_VOLUME_FOLDER + '/multichain.conf', 'w') as conf:
+        with open(self.BC_DATADIR + '/multichain.conf', 'w') as conf:
             conf.write('rpcuser=multichainrpc\n')
             conf.write('rpcpassword=multichainpassword\n')
             conf.write('rpcallowip=0.0.0.0/0')
             conf.close()
-        result['result'] = self.start_node('benchmark', self.headers['Seed-ip'], self.headers['Seed-port'])
+        result['result'] = self.start_node('benchmark', self.BC_DATADIR, self.headers['Seed-ip'], self.headers['Seed-port'])
         self.send_response(200)
         self.send_header('Content-type','application/json')
         self.end_headers()
@@ -61,7 +63,7 @@ class requestHandler(BaseHTTPRequestHandler):
 
     # Return the params.dat file necessary for multichaind with bitcoin protocol
     def do_GET(self):
-        logfilename = self.VM_VOLUME_FOLDER + "/params.dat"             
+        logfilename = self.BC_DATADIR + "/params.dat"             
         with open(logfilename) as pwfile:
             content = pwfile.read()
         self.send_response(200)
@@ -75,8 +77,8 @@ class requestHandler(BaseHTTPRequestHandler):
         start_exec.wait()
         return start_exec.returncode
     
-    def start_node(self, bc_name, seed_ip, seed_port):
-        start_exec = subprocess.Popen(BASH_SCRIPTS_FOLDER + self.start_node_cmd % (bc_name, seed_ip, seed_port), shell=True)
+    def start_node(self, bc_name, bc_datadir, seed_ip, seed_port):
+        start_exec = subprocess.Popen(BASH_SCRIPTS_FOLDER + self.start_node_cmd % (bc_name, bc_datadir, seed_ip, seed_port), shell=True)
         start_exec.wait()
         return start_exec.returncode
 
