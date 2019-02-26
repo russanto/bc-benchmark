ccb852ec7ad703af61c4e24790b2939bf8ca5bc2
Antonio Russo <rjproj@gmail.com>
2019-02-26 11:02:08 +0100

    Adds go logger support and host file parsing

---
+++ ssh_node_manager_hosts.py
@@ -0,0 +1,33 @@
+#!/usr/local/bin/python3
+
+from fabric import Connection
+from ssh_node_manager import NodeManager
+import ipaddress
+
+class NodeManagerHosts(NodeManager):
+    def __init__(self, hosts_filename):
+        for line in open(hosts_filename):
+            stripped = line.strip()
+            self.nodes_ssh_connections.append(Connection(
+                        host=stripped,
+                        user=self.ssh_username,
+                        inline_ssh_env=True
+            ))
+            self.nodes_ips.append(stripped)
+            print("Read: " + stripped + "<--")
+        
+if __name__ == "__main__":
+    import sys, time
+
+    seconds = int(sys.argv[1])
+    hosts_filename = sys.argv[2]
+
+    print("-----> Performing a clean run for %d seconds" % seconds)
+
+    manager = NodeManagerHosts(hosts_filename)
+    manager.clean()
+    manager.create()
+    time.sleep(seconds)
+    manager.stop()
+    
+
