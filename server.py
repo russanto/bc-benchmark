0766c7d5532457a5a4e03a27fa8034aa869a45a5
Antonio Russo <rjproj@gmail.com>
2019-03-27 11:02:17 +0100

    Add server

---
+++ server.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+from ssh_node_manager import NodeManager
+
+app = Flask("NodeManagerServer")
+manager = NodeManager(3, "./conf/manager.conf")
+
+@app.route('/ready/<string:ip_ready>')
+def show_post(ip_ready):
+    manager.add_node_ip(ip_ready)
+    return 'understood'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
