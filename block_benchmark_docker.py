6f1e0f6a89c128a9ff66e0205eede14749b4ecb5
Antonio Russo <rjproj@gmail.com>
2019-03-27 14:08:56 +0100

    Put benchmark workflow in a class

--- block_benchmark_docker.py
+++ block_benchmark_docker.py
@@ -5,28 +5,28 @@ import subprocess
 import sys
 import time
 
-seconds = int(sys.argv[1])
-hosts_filename = sys.argv[2]
+class BlockBenchmark:
+    def __init__(self, start_size=1048576, end_size=2048576, step_size=512000):
+        self.start_size = start_size
+        self.end_size = end_size
+        self.step_size = step_size
 
-start_block_size = 1048576
-end_block_size = 10485760
-step_block_size = 512000
-
-manager = NodeManagerHosts(hosts_filename)
-manager.log_collector_host = sys.argv[3]
-
-manager.connect()
-while start_block_size <= end_block_size:
-    print("-----> Starting benchmark at %d block size" % start_block_size)
-    subprocess.run("sed -i -e '21s/maximum-block-size = [0-9]*/maximum-block-size = %d/' ./conf/params.dat" % start_block_size, shell=True)
-    subprocess.run("docker run -d -p 80:80 -v $PWD/csv:/root/csv --name benchmark-log-collector russanto/bm-btc-multichain-server /root/server %d /root/csv/%d.csv" % (len(manager.nodes_ips), start_block_size), shell=True)
-    manager.clean()
-    manager.create()
-    manager.fullfil()
-    time.sleep(seconds)
-    manager.get_logs(str(start_block_size))
-    manager.stop()
-    subprocess.run("docker stop benchmark-log-collector", shell=True)
-    subprocess.run("docker rm benchmark-log-collector", shell=True)
-    print("-----> Server stopped")
-    start_block_size += step_block_size
+    def start(self, hosts_filename, log_collector_host, sim_time=120):
+        self.manager = NodeManagerHosts(hosts_filename)
+        self.manager.log_collector_host = log_collector_host
+        self.manager.connect()
+        current_size = self.start_size
+        while current_size <= self.end_size:
+            print("-----> Starting benchmark at %d block size" % current_size)
+            subprocess.run("sed -i -e '21s/maximum-block-size = [0-9]*/maximum-block-size = %d/' ./conf/params.dat" % current_size, shell=True)
+            subprocess.run("docker run -d -p 80:80 -v $PWD/csv:/root/csv --name benchmark-log-collector russanto/bm-btc-multichain-server /root/server %d /root/csv/%d.csv" % (len(self.manager.nodes_ips), current_size), shell=True)
+            self.manager.clean()
+            self.manager.create()
+            self.manager.fullfil()
+            time.sleep(sim_time)
+            self.manager.get_logs(str(current_size))
+            self.manager.stop()
+            subprocess.run("docker stop benchmark-log-collector", shell=True)
+            subprocess.run("docker rm benchmark-log-collector", shell=True)
+            print("-----> Server stopped")
+            current_size += self.step_size
