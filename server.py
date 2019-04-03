b36a4fbcd55508380b263f6acf04839032a08337
Antonio Russo <rjproj@gmail.com>
2019-04-03 11:09:58 +0200

    Set status codes in responses

--- server.py
+++ server.py
@@ -13,7 +13,7 @@ if logger_host == "":
     print("[WARNING] Logging block propagation won't work because no logging host has been set")
 
 app = Flask("BC-Orch-Controller")
-app.config["UPLOAD_FOLDER"] = "/root/uploads"
+app.config["UPLOAD_FOLDER"] = "/Users/antonio/Documents/Universita/INSA/bc-benchmark/uploads"
 if not os.path.exists(app.config["UPLOAD_FOLDER"]):
     os.makedirs(app.config["UPLOAD_FOLDER"])
 
@@ -21,8 +21,12 @@ host_manager = HostManager("hosts")
 bp_manager = None
 geth_manager = None
 
+@app.route('/ready')
+def get_ready_count():
+    return jsonify({"count": len(host_manager.get_hosts())})
+
 @app.route('/ready/<string:ip_ready>')
-def show_post(ip_ready):
+def notify_ready(ip_ready):
     host_manager.add_host(ip_ready)
     return jsonify('understood')
 
@@ -31,13 +35,13 @@ def start(nodes_count):
     global bp_manager
     hosts = host_manager.get_hosts()
     if len(hosts) < nodes_count:
-        return jsonify('Not enough nodes ready')
+        return jsonify({"message": 'Not enough nodes ready'}), 403
     elif bp_manager != None:
-        return jsonify('Benchmark already started')
+        return jsonify({"message": 'Benchmark already started'}), 403
     else:
         bp_manager = BlockBenchmarkHandler(hosts[0:nodes_count], logger_host)
         bp_manager.start()
-        return jsonify("started")
+        return jsonify({"message": "started"})
 
 @app.route('/start/ethereum/<int:nodes_count>', methods=['POST'])
 def upload_file(nodes_count):
