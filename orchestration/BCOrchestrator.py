df1489e7edf6a9e4a54c4bad9b08cd476eaee947
Antonio Russo <rjproj@gmail.com>
2019-04-03 15:09:44 +0200

    Add geth support

--- orchestration/BCOrchestrator.py
+++ orchestration/BCOrchestrator.py
@@ -4,6 +4,7 @@ from oslo_utils import encodeutils
 import yaml
 
 from Blockchain import Blockchain
+from GethOpenstack import GethOpenstack
 from MultichainOpenstack import MultichainOpenstack
 from openstack_driver import OpenstackDriver
 
@@ -12,9 +13,16 @@ with open("networks.yaml", "r") as network_config_file:
 
 logging.basicConfig(level=logging.INFO)
 
-multichain = Blockchain("multichain")
 openstack_driver = OpenstackDriver(openstack.connect(cloud="openstack"))
-multichain_os_adapter = MultichainOpenstack()
-multichain_os_adapter.set_driver("insa", openstack_driver)
-multichain.add_instance_manager("openstack", multichain_os_adapter)
-multichain.deploy(network_config["multichain"], "openstack")
+
+# multichain = Blockchain("multichain")
+# multichain_os_adapter = MultichainOpenstack()
+# multichain_os_adapter.set_driver("insa", openstack_driver)
+# multichain.add_instance_manager("openstack", multichain_os_adapter)
+# multichain.deploy(network_config["multichain"], "openstack")
+
+geth = Blockchain("Geth")
+geth_os_adapter = GethOpenstack()
+geth_os_adapter.set_driver("insa", openstack_driver)
+geth.add_instance_manager("openstack", geth_os_adapter)
+geth.deploy(network_config["geth"], "openstack")
